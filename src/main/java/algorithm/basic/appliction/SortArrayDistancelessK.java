4657153397385943813debf2a26424b2aff72137
wangsb <wang2667837@gmail.com>
2022-09-14 00:38:50 +0800

    小根堆-几乎有序问题

---
+++ src/main/java/algorithm/basic/appliction/SortArrayDistancelessK.java
@@ -0,0 +1,12 @@
+package algorithm.basic.appliction;
+
+import java.util.PriorityQueue;
+
+public class SortArrayDistancelessK {
+
+    public void sortedArrDistanceLessK(int[] arr, int k) {
+        //默认小根堆
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+    }
+}
